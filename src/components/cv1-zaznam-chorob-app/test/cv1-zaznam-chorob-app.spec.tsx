8c08ac41f9eecc0bebad3df2861c8f3acb47d052
Mark Bartalos <bartalos86@gmail.com>
2025-04-04 11:06:50 +0200

    initial commit

---
+++ src/components/cv1-zaznam-chorob-app/test/cv1-zaznam-chorob-app.spec.tsx
@@ -0,0 +1,28 @@
+// import { newSpecPage } from '@stencil/core/testing';
+// import { BmAmbulanceWlApp } from '../cv1-zaznam-chorob-wl-app';
+
+describe('cv1-zaznam-chorob-app', () => {
+
+  // it('renders editor', async () => {
+  //   const page = await newSpecPage({
+  //     url: `http://localhost/entry/@new`,
+  //     components: [BmAmbulanceWlApp],
+  //     html: `<bm-ambulance-wl-app base-path="/"></bm-ambulance-wl-app>`,
+  //   });
+  //   page.win.navigation = new EventTarget()
+  //   const child = await page.root.shadowRoot.firstElementChild;
+  //   expect(child.tagName.toLocaleLowerCase()).toEqual ("bm-ambulance-wl-editor");
+
+  // });
+
+  // it('renders list', async () => {
+  //   const page = await newSpecPage({
+  //     url: `http://localhost/ambulance-wl/`,
+  //     components: [BmAmbulanceWlApp],
+  //     html: `<bm-ambulance-wl-app base-path="/ambulance-wl/"></bm-ambulance-wl-app>`,
+  //   });
+  //   page.win.navigation = new EventTarget()
+  //   const child = await page.root.shadowRoot.firstElementChild;
+  //   expect(child.tagName.toLocaleLowerCase()).toEqual("bm-ambulance-wl-list");
+  // });
+});
