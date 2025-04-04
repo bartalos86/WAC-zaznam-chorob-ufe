5ce49419fd637931ea955e4e8af2e8d0bae175ed
Mark Bartalos <bartalos86@gmail.com>
2025-04-04 11:19:28 +0200

    fixed test

--- src/components/cv1-zaznam-chorob-app/test/cv1-zaznam-chorob-app.e2e.ts
+++ src/components/cv1-zaznam-chorob-app/test/cv1-zaznam-chorob-app.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('cv1-zaznam-chorob-app', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<cv1-zaznam-chorob-wl-app></cv1-zaznam-chorob-wl-app>');
+    await page.setContent('<cv1-zaznam-chorob-app></cv1-zaznam-chorob-app>');
 
-    const element = await page.find('cv1-zaznam-chorob-wl-app');
+    const element = await page.find('cv1-zaznam-chorob-app');
     expect(element).toHaveClass('hydrated');
   });
 });
